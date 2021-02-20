e7759bca6721e5d3aaa333ba8458682ef4f9f1a9
Aidan Timson <contact@timmo.xyz>
2021-02-20 17:10:58 +0000

    Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# System Bridge Connector - Python
+
+Python package for connecting to [System Bridge](https://github.com/timmo001/system-bridge).
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import io
+
+from setuptools import setup, find_packages
+
+version = "0.1.0"
+
+setup(
+    name="systembridge",
+    version=version,
+    description="Python package for connecting to System Bridge.",
+    long_description=io.open("README.md", encoding="UTF-8").read(),
+    keywords="system bridge connector",
+    author="Aidan Timson (Timmo)",
+    author_email="contact@timmo.xyz",
+    url="https://github.com/timmo001/system-bridge-connector-py",
+    packages=find_packages(exclude=["tests", "generator"]),
+    install_requires=["aiohttp>=3.7.3"],
+)
