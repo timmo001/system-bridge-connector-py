1c614e607df435c044bf8dc2b6c31e43a569eee0
Aidan Timson <contact@timmo.xyz>
2021-05-16 10:38:41 +0100

    Add license

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     keywords="system bridge connector",
     author="Aidan Timson (Timmo)",
     author_email="contact@timmo.xyz",
+    license="MIT",
     url="https://github.com/timmo001/system-bridge-connector-py",
     packages=find_packages(exclude=["tests", "generator"]),
     install_requires=["aiohttp>=3.7.3"],
