5c26cab0bb2451b8081deed83e986f1ffb29baa4
Aidan Timson <contact@timmo.xyz>
2021-07-24 23:51:37 +0100

    Format

--- systembridge/objects/bluetooth.py
+++ systembridge/objects/bluetooth.py
@@ -9,33 +9,33 @@ from .base import BridgeBase
 
 class Bluetooth(BridgeBase):
     @property
-    def device(self)->str|None:
+    def device(self) -> str | None:
         return self.attributes.get("device")
 
     @property
-    def name(self)->str|None:
+    def name(self) -> str | None:
         return self.attributes.get("name")
 
     @property
-    def manufacturer(self)->str|None:
+    def manufacturer(self) -> str | None:
         return self.attributes.get("manufacturer")
 
     @property
-    def macDevice(self)->str|None:
+    def macDevice(self) -> str | None:
         return self.attributes.get("macDevice")
 
     @property
-    def macHost(self)->str|None:
+    def macHost(self) -> str | None:
         return self.attributes.get("macHost")
 
     @property
-    def batteryPercent(self)->str|None:
+    def batteryPercent(self) -> str | None:
         return self.attributes.get("batteryPercent")
 
     @property
-    def type(self)->str|None:
+    def type(self) -> str | None:
         return self.attributes.get("type")
 
     @property
-    def connected(self)->str|None:
+    def connected(self) -> str | None:
         return self.attributes.get("connected")
