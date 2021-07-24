de6c559b5f38571d49e05a8d23520126e7355d3a
Aidan Timson <contact@timmo.xyz>
2021-07-24 16:07:23 +0100

    Format

--- systembridge/exceptions.py
+++ systembridge/exceptions.py
@@ -4,8 +4,10 @@
 class BridgeException(BaseException):
     """Raise this when something is off."""
 
+
 class BridgeConnectionClosedException(BridgeException):
     """Raise this when connection is closed."""
+
 
 class BridgeAuthenticationException(BridgeException):
     """Raise this when there is an authentication issue."""
